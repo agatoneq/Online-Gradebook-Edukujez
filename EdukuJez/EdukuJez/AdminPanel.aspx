e207c3e372f33485efdca117f79901a3cbcf932e
agatoneq <agata.sobczyk444@wp.pl>
2023-11-26 15:25:43 +0100

    Strona główna Panelu Admina

---
+++ EdukuJez/EdukuJez/AdminPanel.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="Administration Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="AdminPanel.aspx.cs" Inherits="EdukuJez.AdminPanel" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="header Container-Title">
+        <img src="Imgs/Admin_Page_Title.png" class="logo1" style="height: 82px; width: 661px" />
+        <hr />
+    </div>
+    <asp:Table ID="MainAdminTable" runat="server" CellSpacing="20" CssClass="Center-Form Main-Table">
+    </asp:Table>
+</asp:Content>
---
+++ EdukuJez/EdukuJez/AdminPanel.aspx.cs
@@ -0,0 +1,35 @@
+﻿using EdukuJez.Model.Main;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EdukuJez
+{
+    public partial class AdminPanel : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                TableRow row1 = new TableRow();
+                row1.Controls.Add(PanelFactory.MakePanel("Kontami", "#808000", "Main.aspx", this).ConvertToCell());
+                row1.Controls.Add(PanelFactory.MakePanel("Kontami dla Rodziców", "#D2691E", "Main.aspx", this).ConvertToCell());
+                TableRow row2 = new TableRow();
+                row2.Controls.Add(PanelFactory.MakePanel("Planem Zajęć", "#811B1B", "Main.aspx", this).ConvertToCell());
+                row2.Controls.Add(PanelFactory.MakePanel("Kalendarzem", "#9E9A74", "Main.aspx", this).ConvertToCell());
+                TableRow row3 = new TableRow();
+                row3.Controls.Add(PanelFactory.MakePanel("Przedmiotami", "#996515", "Main.aspx", this).ConvertToCell());
+                row3.Controls.Add(PanelFactory.MakePanel("Grupami Zajęciowymi", "#DAA520", "Main.aspx", this).ConvertToCell());
+
+
+                // Dodajemy wiersz do tabeli
+                MainAdminTable.Rows.Add(row1);
+                MainAdminTable.Rows.Add(row2);
+                MainAdminTable.Rows.Add(row3);
+            }
+        }
+    }
+}
