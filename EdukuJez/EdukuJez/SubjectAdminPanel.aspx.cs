bfb0d3393745f4f270f6ca4e3c6676387d855bec
karolinab11 <91892018+karolinab11@users.noreply.github.com>
2023-11-27 17:19:35 +0100

    Add files via upload

---
+++ EdukuJez/EdukuJez/SubjectAdminPanel.aspx.cs
@@ -0,0 +1,55 @@
+﻿using EdukuJez.Model.Main;
+using EdukuJez.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace EdukuJez
+{
+    public partial class SubjectAdminPanel : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                var repoG = new GroupsRepository();
+                var repoU = new UsersRepository();
+                for (int i = 0; i < repoG.GetAll().Count(); ++i)
+                {
+                    DropDownList.Items.Add(repoG.GetAll()[i].GroupName);
+
+                }
+                for (int i = 0; i < repoU.GetAll().Count(); ++i)
+                {
+                    DropDownList.Items.Add(repoU.GetAll()[i].UserName + " " + repoU.GetAll()[i].UserSurname + ":" + repoU.GetAll()[i].UserGroup);
+                }
+            }
+        }
+
+        protected void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            ListBox1.Items.Add(DropDownList.SelectedItem.ToString());
+        }
+
+        protected void ButtonDelete_Click(object sender, EventArgs e)
+        {
+            ListBox1.Items.Remove(ListBox1.SelectedItem);
+        }
+
+        protected void ButtonSubjectAccept_Click(object sender, EventArgs e)
+        {
+            if (TextBoxSubjectName.Text!="") {
+                var repoS = new SubjectsRepository();
+                Subject subject = new Subject();
+                subject.SubjectName = TextBoxSubjectName.Text;
+                //subject.SubjectNameDescription = TextBoxSubjectDescription;
+                repoS.Create(subject);
+                //dodanie grupom uprawnień do przedmiotu
+            }
+        }
+    }
+}
---
+++ EdukuJez/EdukuJez/SubjectAdminPanel.aspx.designer.cs
@@ -0,0 +1,107 @@
+﻿//------------------------------------------------------------------------------
+// <generowane automatycznie>
+//     Ten kod został wygenerowany przez narzędzie.
+//
+//     Zmiany w tym pliku mogą spowodować niewłaściwe zachowanie i zostaną utracone
+//     w przypadku ponownego wygenerowania kodu. 
+// </generowane automatycznie>
+//------------------------------------------------------------------------------
+
+namespace EdukuJez
+{
+
+
+    public partial class SubjectAdminPanel
+    {
+
+        /// <summary>
+        /// Kontrolka LabelSubjectName.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelSubjectName;
+
+        /// <summary>
+        /// Kontrolka TextBoxSubjectName.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxSubjectName;
+
+        /// <summary>
+        /// Kontrolka LabelDescription.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelDescription;
+
+        /// <summary>
+        /// Kontrolka TextBoxSubjectDescription.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxSubjectDescription;
+
+        /// <summary>
+        /// Kontrolka LabelGroupsAndUsers.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelGroupsAndUsers;
+
+        /// <summary>
+        /// Kontrolka DropDownList.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList;
+
+        /// <summary>
+        /// Kontrolka ButtonAdd.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ButtonAdd;
+
+        /// <summary>
+        /// Kontrolka ListBox1.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListBox ListBox1;
+
+        /// <summary>
+        /// Kontrolka ButtonDelete.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ButtonDelete;
+
+        /// <summary>
+        /// Kontrolka ButtonSubjectAccept.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ButtonSubjectAccept;
+    }
+}
