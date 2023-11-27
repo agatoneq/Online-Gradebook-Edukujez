f7ccca207f46b267298fb5e84b5e485b42391d66
KamilaMlyn <91891856+KamilaMlyn@users.noreply.github.com>
2023-11-27 15:16:01 +0100

    Add files via upload

---
+++ EdukuJez/EdukuJez/Model/ServerAccess/Repositories/GradesRepository.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+
+namespace EdukuJez.Repositories { 
+    public class GradesRepository : IRepository<Grade>
+    {
+        List<Grade> GradesList;
+        
+        string CREATE_QUARY = "Select * from Grades";
+
+        public GradesRepository()
+        {
+            GradesList = new List<Grade>();
+            var response = ServerClient.StartConnection().ReturnDataReader(CREATE_QUARY);
+            response.Wait();
+            MapEntities(response.Result);
+        }
+        public GradesRepository(String text)
+        {
+            CREATE_QUARY = "select * from Grades where " + text;
+            GradesList = new List<Grade>();
+            var response = ServerClient.StartConnection().ReturnDataReader(CREATE_QUARY);
+            response.Wait();
+            MapEntities(response.Result);
+        }
+        public Grade GetById(int id) { 
+            throw new NotImplementedException(); 
+        }
+        public List<Grade> GetAll() {
+            return GradesList;
+        }
+        public void Create(Grade entity) {
+            throw new NotImplementedException(); 
+        }
+        public void Update(Grade entity) {
+            throw new NotImplementedException(); 
+        }
+        public void Delete(Grade entity) {
+            throw new NotImplementedException();
+        }
+
+        //Mapowanie
+        private void MapEntities(SqlDataReader reader)
+        {
+            if (reader != null)
+                while (reader.Read())
+            {
+                Grade grade = new Grade();
+
+                grade.Id = reader.GetInt32(0);
+                grade.GradeValue = reader.GetInt32(1);
+                grade.GradeWeight = reader.GetInt32(2);
+                grade.GradeIDSubject = reader.GetInt32(3);
+       
+                GradesList.Add(grade);
+            }
+        }
+
+        //Wyszukiwanie
+        public List<int> getGrades(int id)
+        {
+            List<int> gradesList = new List<int>();
+            for (int i = 0; i < GradesList.Count(); ++i) {
+                if (GradesList[i].Id == id) {
+                    gradesList.Add(GradesList[i].GradeValue);
+                }
+            }
+            return gradesList;
+        }
+       
+    }
+}
---
+++ EdukuJez/EdukuJez/Model/ServerAccess/Repositories/SubjectsRepository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Xml;
+
+namespace EdukuJez.Repositories 
+{
+    public class SubjectsRepository : IRepository<Subject>
+    {
+        List<Subject> SubjectList;
+        static string CREATE_QUARY = "Select * from Subjects";
+        public SubjectsRepository()
+        {
+            SubjectList = new List<Subject>();
+            var response = ServerClient.StartConnection().ReturnDataReader(CREATE_QUARY);
+            response.Wait();
+            MapEntities(response.Result);
+        }
+
+        public SubjectsRepository(String sqlText)
+        {
+            SubjectList = new List<Subject>();
+            var response = ServerClient.StartConnection().ReturnDataReader("select * from User where " + sqlText);
+            response.Wait();
+            MapEntities(response.Result);
+        }
+        //implementacja interfejsu
+        public Subject GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public List<Subject> GetAll()
+        {
+            return SubjectList;
+        }
+        public void Create(Subject entity)
+        {
+            CREATE_QUARY = "insert into Subjects values ('"+entity.SubjectName+"')";
+            var response = ServerClient.StartConnection().ReturnDataReader(CREATE_QUARY);
+        }
+        public void Update(Subject entity)
+        {
+            throw new NotImplementedException();
+        }
+        public void Delete(Subject entity)
+        {
+            throw new NotImplementedException();
+        }
+        //Mapowanie
+        private void MapEntities(SqlDataReader reader)
+        {
+            if(reader != null)
+            while (reader.Read())
+            {
+                Subject subject = new Subject();
+
+                subject.Id = reader.GetInt32(0);
+                subject.SubjectName = reader.GetString(1);
+
+                SubjectList.Add(subject);
+            }
+        }
+    }
+}
