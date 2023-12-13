f4745e3fdab353077640c6cbdad560917419b7dd
EustachyL <57866931+EustachyL@users.noreply.github.com>
2023-12-13 22:43:47 +0100

    pierwszy OEM wraz z migracją

---
+++ EdukuJez/EdukuJez/Model/Data/BaseContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.EntityFrameworkCore;
+using EdukuJez.Repositories;
+using EdukuJez;
+
+namespace EdukuJez
+{
+    public class BaseContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<ClassC> ClassC { get; set; }
+        public DbSet<Grade> Grade { get; set; }
+        public DbSet<Group> Group { get; set; }
+        public DbSet<Subject> Subject { get; set; }
+        public DbSet<SubjView> SubjView { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Konfiguracja połączenia z bazą danych
+            optionsBuilder.UseSqlServer(ServerClient.CONSTRING);
+        }
+    }
+}
--- EdukuJez/EdukuJez/Model/ServerAccess/DBEntity/EntityBase.cs
+++ EdukuJez/EdukuJez/Model/ServerAccess/DBEntity/EntityBase.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace EdukuJez.Repositories
 {
     public abstract class EntityBase
     {
+        [Key]
         public int Id { get; set; }
     }
 }
