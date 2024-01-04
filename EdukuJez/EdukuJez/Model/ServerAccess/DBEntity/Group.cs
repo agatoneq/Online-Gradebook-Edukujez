f255ed1fff4988cd17869cbf8d24e204eca11068
EustachyL <57866931+EustachyL@users.noreply.github.com>
2024-01-04 21:56:31 +0100

    Dodanie dedykowanych grup dla przedmiotów w bazie danych

--- EdukuJez/EdukuJez/Model/ServerAccess/DBEntity/Group.cs
+++ EdukuJez/EdukuJez/Model/ServerAccess/DBEntity/Group.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,12 @@ namespace EdukuJez.Repositories
 {
     public class Group : EntityBase
     {
+        [Required]
         public string Name { get; set; }
         public Group ParentGroup { get; set; }
         public User Educator { get; set; }
-        public ICollection<Group>  ChildGroups { get; set; } = new List<Group>();
+        public ICollection<Subject> SubjectsStudents { get; set; } = new List<Subject>();
+        public ICollection<Subject> SubjectsTeachers { get; set; } = new List<Subject>();
         public ICollection<GroupUser> Users { get; set; } = new List<GroupUser>();
         public ICollection<ClassC> Classes { get; set; } = new List<ClassC>();
         public ICollection<MessageGroups> Messages { get; set; } = new List<MessageGroups>();
