50887b35382aecd91697c74775e6408192bbea8a
EustachyL <57866931+EustachyL@users.noreply.github.com>
2024-01-28 14:19:35 +0100

    Strony Przedmiotów (na razie szablon)

---
+++ EdukuJez/EdukuJez/Model/Main/SubjectManager.cs
@@ -0,0 +1,14 @@
+﻿using EdukuJez.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace EdukuJez.Model.Main
+{
+    public static class SubjectManager
+    {
+        public static Subject Subject{get; set;}
+    }
+}
---
+++ EdukuJez/EdukuJez/SubjectContentPage.aspx.designer.cs
@@ -0,0 +1,80 @@
+﻿//------------------------------------------------------------------------------
+// <generowany automatycznie>
+//     Ten kod został wygenerowany przez narzędzie.
+//
+//     Modyfikacje tego pliku mogą spowodować niewłaściwe zachowanie i zostaną utracone
+//     w przypadku ponownego wygenerowania kodu. 
+// </generowany automatycznie>
+//------------------------------------------------------------------------------
+
+namespace EdukuJez
+{
+
+
+    public partial class SubjectContentPage
+    {
+
+        /// <summary>
+        /// Kontrolka SubjectNameLabel.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label SubjectNameLabel;
+
+        /// <summary>
+        /// Kontrolka AttachmentPanel.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel AttachmentPanel;
+
+        /// <summary>
+        /// Kontrolka AttachmentLabel.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label AttachmentLabel;
+
+        /// <summary>
+        /// Kontrolka AttachmentTable.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Table AttachmentTable;
+
+        /// <summary>
+        /// Kontrolka ActivitesPanel.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel ActivitesPanel;
+
+        /// <summary>
+        /// Kontrolka ActivitesLabel.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ActivitesLabel;
+
+        /// <summary>
+        /// Kontrolka ActivitesTable.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Table ActivitesTable;
+    }
+}
