e9177f0d4f37c33f443e1e527d64fc15c7ca6c4a
agatoneq <agata.sobczyk444@wp.pl>
2024-01-28 17:11:01 +0100

    remark-remarks błąd

--- EdukuJez/EdukuJez/Remarks.aspx.cs
+++ EdukuJez/EdukuJez/Remarks.aspx.cs
@@ -6,29 +6,83 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
 
 namespace EdukuJez
 {
     public partial class Remarks : System.Web.UI.Page
     {
-        public GradesRepository repoGrades = new GradesRepository();
-        public SubjectsRepository repoSubj = new SubjectsRepository();
-        public SubjViewRepository View = new SubjViewRepository();
-        public GroupsRepository repoGroups = new GroupsRepository();
+        private Remark newRemark = new Remark();
+        readonly RemarkRepository remarkRepo = new RemarkRepository();
+        //public SubjectsRepository repoSubj = new SubjectsRepository();
+        //public SubjViewRepository View = new SubjViewRepository();
+
+        readonly GroupsRepository groupsRepo = new GroupsRepository();
+        readonly UsersRepository userRepo = new UsersRepository();
+        readonly GroupUsersRepository groupUserRepo = new GroupUsersRepository();
         String permission;
         //User currentuser = UserSession.GetSession().user;
+        List<Group> groups = new List<Group>();
+        
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                groups = groupsRepo.Table.ToList();
+                StudentsGroupsList.DataSource = groups.Where(x => x.ParentGroup != null).Select(x => x.Name);
+                StudentsGroupsList.DataBind();
 
+                UploadStudentsList(groups);
+
+            }
         }
         
+        protected void UploadStudentsList(List<Group> groups)
+        {
+            var groupsId = groups.Where(x => x.Name == StudentsGroupsList.Text).Select(x => x.Id);
+            var groupId = groupsId.FirstOrDefault();
+
+            List<User> users = userRepo.Table.ToList();
+            List<GroupUser> groupUserList = groupUserRepo.Table.Include(u => u.User).Include(g => g.Group).ToList();
+
+            List<int> studentsId = groupUserList.Where(x => x.Group != null && x.Group.Id == groupId && x.User != null).Select(x => x.User.Id).ToList();
+
+
+            StudentsList.DataSource = users.Where(x => studentsId.Contains(x.Id)).Select(user => $"{user.UserName} {user.UserSurname}");
+
+            StudentsList.DataBind();
+        }
+
         protected void AddNewRemarkButton_Click(object sender, EventArgs e)
         {
+            var Session = UserSession.GetSession();
+
+            if (NewRemarkTextBox.Text != null)
+            {
+                newRemark.Contents = NewRemarkTextBox.Text;
+                remarkRepo.Insert(newRemark);
+
+                //var teachersId = Session.UserId;
+                //var studentsFullName = StudentsList.SelectedValue;
+                //newRemark.Submitter = userRepo.Table.First(x => x.Id == teachersId);
+                //newRemark.Student = userRepo.Table.First(x => (x.UserName + " " + x.UserSurname) == studentsFullName);
+
+            }
         }
 
         protected void StudentsGroupsListSelectedIndexChanged(object sender, EventArgs e)
         {
+            groups = groupsRepo.Table.ToList();
+            UploadStudentsList(groups);
+        }
+        protected void NewRemarkBoxChanged(object sender, EventArgs e)
+        {
+            if(NewRemarkTextBox.Text != null)
+            {
+                AddNewRemarkButton.Enabled = true;
+            }
         }
 
         protected void GoBackButton_Click(object sender, EventArgs e)
